fd322fde4d22f2bfe671e825b20e807dfc0ec09b
erlebach <gordon.erlebach@gmail.com>
2019-09-24 10:58:07 -0400

    Updated UFOController.cs . Use ASDF keys to move.
    Mouse on Mac is not controlling UFO. Do not know.

---
+++ 2D-UFO/Assets/scripts/UFOController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOController : MonoBehaviour {
+    // A public variables is visible in the inspector
+    public float speed;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D> ();
+        speed = 0f;
+    }
+
+    void FixedUpdate () {
+        print ("gordon");
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        // AddForce
+
+        rigidbody.AddForce (new Vector3 (moveHorizontal, moveVertical, 0));
+    }
+    // Update is called once per frame
+    void Update () {
+        // Method 1
+        //float xpos = transform.position.x + speed;
+        //transform.position = new Vector2 (xpos, 0f);
+
+        // Method 2
+        //transform.position = new Vector2 (transform.position.x + speed, 0f);
+
+    }
+}
