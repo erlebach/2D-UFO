33c440536c9f4c6c574475a7b0fc0022111c4ca2
erlebach <gordon.erlebach@gmail.com>
2019-09-25 08:09:56 -0400

    Added GUI text, and updated UFOController.cs
    Count updates properly.

--- 2D-UFO/Assets/scripts/UFOController.cs
+++ 2D-UFO/Assets/scripts/UFOController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // for GUI elements
 
 public class UFOController : MonoBehaviour {
     // A public variables is visible in the inspector
     public float speed;
     private Rigidbody2D rigidbody;
+    public Text score_reg;
+    private int score;
 
     // Start is called before the first frame update
     void Start () {
         rigidbody = GetComponent<Rigidbody2D> ();
+        //score_reg.text = "0000";
+        score = 0;
     }
 
     void FixedUpdate () {
@@ -35,6 +40,13 @@ public class UFOController : MonoBehaviour {
     private void OnTriggerEnter2D (Collider2D other) {
         if (other.gameObject.CompareTag ("PickUp")) {
             other.gameObject.SetActive (false); // do not destroy
+            score += 1;
+            print (score);
+            setScore ();
         }
+    }
+
+    private void setScore () {
+        score_reg.text = score.ToString ();
     }
 }
