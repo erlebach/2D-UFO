862390358709d1915930b3104c786b826e31474c
erlebach <gordon.erlebach@gmail.com>
2019-10-06 23:08:23 -0400

    Restructured source code. Still does not function as expected.
    Explosion enteres a loop. Somehow, I am not disabling isExplosion variable.
    I also changed the parameters in the animator.

--- Animation_demo/Assets/BoySuitController.cs
+++ Animation_demo/Assets/BoySuitController.cs
@@ -6,6 +6,8 @@ public class BoySuitController : MonoBehaviour {
     Rigidbody2D myRigidBody;
     public float speed;
     public Animator myAnimator;
+    bool keyDown;
+    float xpos;
 
     // Start is called before the first frame update
     void Awake () {
@@ -15,9 +17,28 @@ public class BoySuitController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        keyDown = Input.GetKeyDown (KeyCode.Space);
+        xpos = Input.GetAxis ("Horizontal");
 
-        bool keyDown = Input.GetKeyDown (KeyCode.Space);
-        float xpos = Input.GetAxis ("Horizontal");
+        // No explosion while running
+        if (keyDown && xpos == 0f) {
+            explosion ();
+        } else {
+            myAnimator.SetBool ("isExplosion", false);
+        }
+        horizMotion ();
+    }
+
+    void explosion () {
+        myAnimator.SetBool ("isExplode", true);
+        System.Threading.Thread.Sleep (100);
+        myAnimator.SetBool ("isExplosion", false);
+        // should happen AFTER the explosion. Here it does not
+        resetPosition ();
+
+    }
+
+    void horizMotion () {
 
         if (xpos >.5f) {
             myAnimator.SetBool ("isRunning", true);
@@ -27,16 +48,15 @@ public class BoySuitController : MonoBehaviour {
             myAnimator.SetBool ("isRunning", true);
             transform.localScale = new Vector3 (-1f, 1f, 1f);
             transform.position += new Vector3 (xpos, 0f, 0f) * speed * Time.deltaTime;
-        } else {
+        } else { // idle
             myAnimator.SetBool ("isRunning", false);
-        }
-
-        if (keyDown == true) {
-            myAnimator.SetTrigger ("explosion");
         }
     }
 
-    public void resetPosition () {
+    void resetPosition () {
+        // the next line would prevent explosion. (code runs during explosion)
+        // So I could sleep for length of the explosion. 
+        //myAnimator.SetBool ("isExplode", false);
         transform.position = new Vector3 (1.2f, -.62f, 0f);
     }
 }
